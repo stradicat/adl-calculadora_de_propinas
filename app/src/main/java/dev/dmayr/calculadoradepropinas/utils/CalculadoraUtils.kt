62b266bd006173c9064d6ab6ec01c0c950a8aeea
Dennis Mayr <dmayr.dev@gmail.com>
2025-07-23 10:14:39 -0400

    CalculadoraUtils: return en Int redondeado, añadido chequeo de valores

--- app/src/main/java/dev/dmayr/calculadoradepropinas/utils/CalculadoraUtils.kt
+++ app/src/main/java/dev/dmayr/calculadoradepropinas/utils/CalculadoraUtils.kt
@@ -1,7 +1,9 @@
 package dev.dmayr.calculadoradepropinas.utils
 
 class CalculadoraUtils {
-    fun calcularPropina(cuenta: Double, porcentaje: Double): Double {
-        return kotlin.math.round((porcentaje / 100) * cuenta)
+    fun calcularPropina(cuenta: Int, porcentaje: Int): Int {
+        return if (cuenta > 0 && porcentaje > 0) {
+            kotlin.math.round((cuenta * porcentaje / 100).toDouble()).toInt()
+        } else 0
     }
 }
